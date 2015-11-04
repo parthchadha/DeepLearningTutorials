d6d1b19e3bb214103d7f73f796282061099afcea
Parth Chadha <pchadha@andrew.cmu.edu>
2015-11-04 12:12:53 +0800

    test

--- code/mlp.py
+++ code/mlp.py
@@ -393,13 +393,17 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
                            'best model %f %%') %
                           (epoch, minibatch_index + 1, n_train_batches,
                            test_score * 100.))
-                    
+
+                with open('best_model.pkl', 'w') as f:
+                    cPickle.dump(MLP.hiddenLayer.w.get_value(), f,-1)
+                    #cPickle.dump(layer3.b.get_value(), f,-1)
 
             if patience <= iter:
                 done_looping = True
                 break
 
     end_time = timeit.default_timer()
+
 
     print(('Optimization complete. Best validation score of %f %% '
            'obtained at iteration %i, with test performance %f %%') %
